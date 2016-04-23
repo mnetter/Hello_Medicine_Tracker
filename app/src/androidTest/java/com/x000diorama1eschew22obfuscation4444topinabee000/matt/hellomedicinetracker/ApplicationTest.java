c96185264acf784fa2795e5e0dcdc9676391d192
Matt <matt_netter@hotmail.com>
2016-04-23 18:49:08 -0400

    last commit until upload

---
+++ app/src/androidTest/java/com/x000diorama1eschew22obfuscation4444topinabee000/matt/hellomedicinetracker/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.x000diorama1eschew22obfuscation4444topinabee000.matt.hellomedicinetracker;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/com/x000diorama1eschew22obfuscation4444topinabee000/matt/hellomedicinetracker/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.x000diorama1eschew22obfuscation4444topinabee000.matt.hellomedicinetracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
